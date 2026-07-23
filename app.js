ef68e8c5a2e502173bfec99ed4fe3b095f5b75ed
noviernawatiopy <67525122+noviernawatiopy@users.noreply.github.com>
2026-07-23 10:19:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const video = document.getElementById("camera");
 const status = document.getElementById("status");
 const btn = document.getElementById("btnStart");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const hasilQR = document.getElementById("hasilQR");
 
 btn.addEventListener("click", startCamera);
 
@@ -17,7 +20,32 @@ async function startCamera() {
     status.textContent = "✅ Kamera berhasil dibuka";
     btn.disabled = true;
     btn.textContent = "Kamera Aktif";
-  } catch (err) {
+    scanQR();
+  } 
+  function scanQR() {
+  if (video.readyState === video.HAVE_ENOUGH_DATA) {
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    const code = jsQR(
+      imageData.data,
+      canvas.width,
+      canvas.height
+    );
+
+    if (code) {
+      hasilQR.textContent = "QR: " + code.data;
+      return;
+    }
+  }
+
+  requestAnimationFrame(scanQR);
+}catch (err) {
     console.error(err);
     status.textContent = "❌ Gagal membuka kamera";
   }
