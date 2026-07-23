7440b966b842a5759f39e8433a46f5fdde981cf8
noviernawatiopy <67525122+noviernawatiopy@users.noreply.github.com>
2026-07-23 09:57:53 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const video = document.getElementById("camera");
+const status = document.getElementById("status");
+const btn = document.getElementById("startBtn");
+
+btn.addEventListener("click", startCamera);
+
+async function startCamera() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: "environment"
+      },
+      audio: false
+    });
+
+    video.srcObject = stream;
+    status.textContent = "✅ Kamera berhasil dibuka";
+    btn.disabled = true;
+    btn.textContent = "Kamera Aktif";
+  } catch (err) {
+    console.error(err);
+    status.textContent = "❌ Gagal membuka kamera";
+  }
+}
